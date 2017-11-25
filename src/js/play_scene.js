addf48589e2f84f6ae869786931ad8afe6244ead
bornunez <bornunez@ucm.es>
2017-11-25 13:44:28 +0100

    Prueba de colisión funcional.
    Modificado Player para que utilice velocidades y pueda ser detectado por las colisiones.

--- src/js/play_scene.js
+++ src/js/play_scene.js
@@ -22,32 +22,28 @@ var PlayScene = {
 
     
     this.game.physics.enable(this.link);
-
     this.link.body.collideWorldBounds = true;
     this.link.body.bounce.setTo(1, 1);
 
- 
+    this.link.body.moves = true;
  
     this.zelda = new Zelda("Zelda",this.game.world.centerX+100,this.world.game.world.centerY);
     this.game.world.addChild(this.zelda);
     
     this.game.physics.enable(this.zelda);
 
-
-
-
     this.zelda.body.collideWorldBounds = true;
-    this.zelda.body.checkCollision.up = false;
-    this.zelda.body.checkCollision.down = false;
-    this.zelda.body.immovable = false;
-    
+    this.zelda.body.checkCollision.up = true;
+    this.zelda.body.checkCollision.down = true;
+    this.zelda.body.immovable = true;
+
   },
   update: function(){
+    this.game.physics.arcade.collide(this.link, this.zelda);
 
     if(this.game.physics.arcade.collide(this.link, this.zelda)) {
      console.log("COLISION") 
     }
-
   }
 };
 
@@ -86,13 +82,13 @@ Hero.prototype.update= function(){
   if(PlayScene.upKey.isDown){
     if(this.dir === 'None')
       this.dir = 'Up'
-    this._velY = -1;
+    this._velY = -200;
     this.move = true;
   }
   else if(PlayScene.downKey.isDown){
     if(this.dir === 'None')
       this.dir = 'Down'
-    this._velY = 1;
+    this._velY = 200;
     this.move = true;
   }
   else {
@@ -104,11 +100,11 @@ Hero.prototype.update= function(){
   //X Axis
   if(PlayScene.leftKey.isDown){
     this.move = true;
-    this._velX = -1;
+    this._velX = -200;
   }
   else if (PlayScene.rightKey.isDown){
     this.move = true;
-    this._velX = 1;
+    this._velX = 200;
   }
   else{
     if(this._velY != 0)
@@ -118,9 +114,14 @@ Hero.prototype.update= function(){
   }
   //Movemos
   if(this.move){
-    this.x += this._velX * this._vel;
-    this.y += this._velY * this._vel;
+    this.body.velocity.x = this._velX;
+    this.body.velocity.y = this._velY;
   }
+  else {
+    this.body.velocity.x = 0;
+    this.body.velocity.y = 0;
+  }
+
   //Objeto(Disparar)
   if(PlayScene.space.isDown){
     if(this.canShoot)
@@ -128,10 +129,10 @@ Hero.prototype.update= function(){
   }
 }
 Hero.prototype.shoot = function(){
-  var arrow = new Arrow(PlayScene.game,this.x,this.y,this._velX,this._velY);
-  PlayScene.game.world.addChild(arrow);
+  var arrow = new Arrow(PlayScene.game,this.x,this.y,this._velX,this._velY); //velx y vely son el fallo, si se ponen a uno van
+  PlayScene.game.world.addChild(arrow);                            //Habria que poner su movimiento en funcion a velocidad   
   this.canShoot = false;
-  PlayScene.game.time.events.add(Phaser.Timer.SECOND * 1, this.shootCD, this);
+  PlayScene.game.time.events.add(Phaser.Timer.SECOND  * 1, this.shootCD, this);
 }
 Hero.prototype.shootCD = function(){
   this.canShoot = true;
@@ -141,7 +142,6 @@ Hero.prototype.shootCD = function(){
 function Arrow(game,X, Y, VELX, VELY){
 
   Phaser.Sprite.call(this,game, X, Y, 'link');
-
   this.vel = 5;
   this.x = X;
   this.y = Y;
