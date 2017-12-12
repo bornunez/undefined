401d0167ae20f1d98ea74797e0a03b261b512051
Miguel <mgremo@ucm.es>
2017-12-12 10:22:59 +0100

    Tilemap hecho

---
+++ src/mapas/zeldatileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Zelda" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="156" columns="13">
+ <image source="../images/TileSet32.png" trans="ff00ff" width="430" height="397"/>
+</tileset>
